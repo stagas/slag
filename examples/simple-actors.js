3f408711c237056deffa6a75c121c76e275b8737
George Stagas <gstagas@gmail.com>
2012-04-21 02:01:53 +0300

    Basic Actor stuff

---
+++ examples/simple-actors.js
@@ -0,0 +1,68 @@
+var util = require('util')
+var slag = require('../')
+
+function Point () {
+  slag.Actor.call(this)
+  this.x = 20
+  this.y = 0
+  this.vx = 0
+  this.vy = 0
+  this.a = 0
+  this.speed = 2
+}
+
+util.inherits(Point, slag.Actor)
+
+Point.prototype.advance = function () {
+  this.vx = this.speed * Math.cos(this.a * (Math.PI / 180))
+  this.vy = this.speed * Math.sin(this.a * (Math.PI / 180))
+  this.x += this.vx
+}
+
+Point.prototype.update = function () {
+  if (this.speed <= 1) {
+    this.a = 180 - this.a
+  }
+  if (this.a > 90) {
+    this.speed = (this.x) * 0.8
+  } else {
+    this.speed = (40 - this.x) * 0.8
+  }
+  this.advance()
+  return [ this.x ]
+}
+
+Point.prototype.render = function (s) {
+  var len = (s[0] | 0) + 1
+  process.stdout.write(
+    '\033[s\033[14C'
+  + 'POS:' + s[0].toFixed(global.DECIMAL_DIGITS)
+  + new Array(len).join(' ')
+  + '*'
+  + new Array(Math.max(0, 42 - len)).join(' ')
+  + '\033[u'
+  )
+}
+
+function FrameCounter () {
+  slag.Actor.call(this)
+  this.frames = 0
+}
+
+util.inherits(FrameCounter, slag.Actor)
+
+FrameCounter.prototype.update = function () {
+  this.frames++
+  return [ this.frames ]
+}
+
+FrameCounter.prototype.render = function (s) {
+  process.stdout.write(
+    '\033[s'
+  + 'FRAME:' + s[0]
+  + '\033[u'
+  )
+}
+
+exports.Point = Point
+exports.FrameCounter = FrameCounter
--- examples/simple.js
+++ examples/simple.js
@@ -1,13 +1,30 @@
+var util = require('util')
 var slag = require('../')
+var actors = require('./simple-actors')
 
+// create world
 var world = slag.createWorld()
+
+// create a point
+var point = new actors.Point()
+
+// join point actor in world
+world.join(point)
+
+// create and join frame counter actor
+world.join(new actors.FrameCounter)
+
+// prepare world
 world.prepare()
-world.fps = 1
 
-console.log(world)
+// set fps
+world.fps = 4
 
-world.loop(function (alpha) {
-  console.log('alpha:', alpha)
+console.log('WORLD:', world)
+
+// loop world
+world.loop(function () {
+  this.render()
 }, function () {
-  console.log('******** frame:', this.frame)
+  this.update()
 })
